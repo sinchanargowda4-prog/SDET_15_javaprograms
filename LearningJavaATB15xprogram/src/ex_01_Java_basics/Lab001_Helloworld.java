e88da4e8ef3a0ad0bbdca5611b9467f460016df6
sinchana <sinchana@LAPTOP-E1TE4TUR>
2025-12-10 23:18:39 +0530

    sammple program

---
+++ LearningJavaATB15xprogram/src/ex_01_Java_basics/Lab001_Helloworld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ex_01_Java_basics;
+
+/**
+ * 
+ */
+public class Lab001_Helloworld {
+
+	/**
+	 * 
+	 */
+	public Lab001_Helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LearningJavaATB15xprogram/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module LearningJavaATB15xprogram {
+}
