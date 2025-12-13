6321827a43a8d023d49f0ea1bce5e2701b5d8a72
sinchana <sinchana@LAPTOP-E1TE4TUR>
2025-12-13 23:36:29 +0530

    sample program

--- LearningJavaATB15xprogram/src/ex_01_Java_basics/Lab001_Helloworld.java
+++ LearningJavaATB15xprogram/src/ex_01_Java_basics/Lab001_Helloworld.java
@@ -9,13 +9,6 @@ package ex_01_Java_basics;
 public class Lab001_Helloworld {
 
 	/**
-	 * 
-	 */
-	public Lab001_Helloworld() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
