86d3e076f9c6d35334bdcb37ead4a1220a14b4a9
sinchana <sinchana@LAPTOP-E1TE4TUR>
2025-12-17 23:00:27 +0530

    sample programs for datatypes & printf

---
+++ LearningJavaATB15xprogram/src/ex_01_Java_basics/Lab003_datatype.java
@@ -0,0 +1,33 @@
+package ex_01_Java_basics;
+
+public class Lab003_datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =10;
+		float b = 19.09f;
+		double c = 1.0009878767d;
+		char c1 = 'C';
+		boolean b1= false;
+		final int g =10;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(b1);
+		
+//		printf
+		System.out.printf("a value is %d",a);
+		System.out.printf("b value is %f",b);
+		System.out.printf("c value is %f",c);
+		System.out.printf("c1 value is %s",c1);
+		System.out.printf("b1 value is %b",b1);
+		
+//		constant
+//		g =+ 20;
+		
+		
+	}
+
+}
---
+++ LearningJavaATB15xprogram/src/ex_01_Java_basics/Lab004_printf.java
@@ -0,0 +1,33 @@
+package ex_01_Java_basics;
+
+public class Lab004_printf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//        int a=10;
+//        System.out.printf("a value is %d",a);
+        int i=9;
+        System.out.printf("%d *1 = %d", i, i*1);
+        System.out.println();
+        System.out.printf("%d *2 = %d", i, i*2);
+        System.out.println();
+        System.out.printf("%d *3 = %d", i ,i*3);
+        System.out.println();
+        System.out.printf("%d *4 = %d", i, i*4);
+        System.out.println();
+        System.out.printf("%d *5 = %d", i, i*5);
+        System.out.println();
+        System.out.printf("%d *6 = %d", i , i*6);
+        System.out.println();
+        System.out.printf("%d *7 = %d", i , i*7);
+        System.out.println();
+        System.out.printf("%d *8 = %d", i , i*8);
+        System.out.println();
+        System.out.printf("%d *9 = %d", i , i*9);
+        System.out.println();
+        System.out.printf("%d *10 = %d", i , i*10);
+        System.out.println();
+        
+	}
+
+}
